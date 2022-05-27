6abb52419378ad2f01ff7b2cc5b0e8458774c01c
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-05-27 10:56:57 +0200

    refactoring and error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,19 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://raw2002:soxaOFaVBk5IWsWj@cluster0.7ueiz.mongodb.net/events?retryWrites=true&w=majority'
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection('newsletter').insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,14 +23,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://raw2002:soxaOFaVBk5IWsWj@cluster0.7ueiz.mongodb.net/events?retryWrites=true&w=majority'
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection('newsletter').insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' });
+      return;
+    }
 
     res.status(201).json({ message: 'Signed up!' });
   }
